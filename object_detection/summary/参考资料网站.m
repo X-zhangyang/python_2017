6b5a2c2c20386a47d244432f104f0740e5ad03fe
X-zhangyang <33534662+X-zhangyang@users.noreply.github.com>
2017-11-10 11:09:33 +0800

    Add files via upload

---
+++ object_detection/summary/参考资料网站.m
@@ -0,0 +1,18 @@
+﻿pascal voc数据集分析                http://m.blog.csdn.net/zhangjunbob/article/details/52769381   
+
+单&双隐藏层自编码器设计              http://www.cnblogs.com/hellcat/p/6977830.html 
+
+
+
+anchor的解释和框框选取       http://blog.csdn.net/sinat_31802439/article/details/52593246
+ 
+
+
+
+
+
+
+
+
+
+
